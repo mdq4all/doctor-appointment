1e21078a939441b10a5b102683c1fd58c8e956f7
Damian <LaBancariaDevs@gmail.com>
2024-04-12 10:41:54 -0300

    probando build en next

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'media.graphassets.com',
-              port: '',
-              pathname: '',
-            },
-            {
-              protocol: 'https',
-              hostname: 'lh3.googleusercontent.com',
-              port: '',
-              pathname: '',
-            },
-          ],
-    }
+    // images:{
+    //     remotePatterns: [
+    //         {
+    //           protocol: 'https',
+    //           hostname: 'media.graphassets.com',
+    //           port: '',
+    //           pathname: '',
+    //         },
+    //         {
+    //           protocol: 'https',
+    //           hostname: 'lh3.googleusercontent.com',
+    //           port: '',
+    //           pathname: '',
+    //         },
+    //       ],
+    // }
 };
 
 export default nextConfig;
